eac078bd9dbf07f8ebda376a2a33e15d14ca0ab0
Jasper van der Laan <75309576+Jasper85125@users.noreply.github.com>
2024-03-27 11:19:59 +0100

    added Informatie file

---
+++ Project/Presentation/Informatie.cs
@@ -0,0 +1,10 @@
+static public class Informatie
+{
+    static public void Info()
+    {
+        Console.WriteLine("Ons pannenkoeken restaurant is een bus waar je op");
+        Console.WriteLine("meerderen plekken kan instappen zodat je kan eten terwijl de bus een rondje rijdt."); 
+        Console.WriteLine("En op deze app/site kan je reserveren waar en wanneer je met ons mee wil rijden.");
+    }
+
+}
--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -22,9 +22,7 @@ static class Menu
         }
         else if (input == "3")
         {
-            Console.WriteLine("Ons pannenkoeken restaurant is een bus waar je op");
-            Console.WriteLine("meerderen plekken kan instappen zodat je kan eten terwijl de bus een rondje rijdt."); 
-            Console.WriteLine("En op deze app/site kan je reserveren waar en wanneer je met ons mee wil rijden.");
+            Informatie.Info();
         }
         else if (input == "4")
         {
