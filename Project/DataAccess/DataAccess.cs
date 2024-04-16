c842bd8ce572403fbd92922cc84238f339a8c244
Amer Alhasoun <ameralhasoun@gmail.com>
2024-04-16 15:14:10 +0200

    DataAccess try catch error

--- Project/DataAccess/DataAccess.cs
+++ Project/DataAccess/DataAccess.cs
@@ -6,16 +6,34 @@ static class DataAccess<T>
 
     public static List<T> LoadAll(string fileName)
     {
+        Console.WriteLine(typeof(T).Name);
         string path = System.IO.Path.Combine(basePath, $"{fileName}.json");
-        string json = File.ReadAllText(path);
-        return JsonSerializer.Deserialize<List<T>>(json);
+        List<T> ListToReturn = null;
+        try
+        {
+            string json = File.ReadAllText(path);
+            ListToReturn = JsonSerializer.Deserialize<List<T>>(json);
+            return ListToReturn;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return ListToReturn;
     }
 
     public static void WriteAll(List<T> items, string fileName)
     {
-        string path = System.IO.Path.Combine(basePath, $"{fileName}.json");
-        var options = new JsonSerializerOptions { WriteIndented = true };
-        string json = JsonSerializer.Serialize(items, options);
-        File.WriteAllText(path, json);
+        try
+        {
+            string path = System.IO.Path.Combine(basePath, $"{fileName}.json");
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(items, options);
+            File.WriteAllText(path, json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
