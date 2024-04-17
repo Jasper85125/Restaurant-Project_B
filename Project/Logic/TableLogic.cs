484ad53bf7b2ec8f906287b3f47f60bac9e3c447
ThijsPR <tavandebelt@gmail.com>
2024-04-17 13:56:00 +0200

    can rijen in tabellen selecteren

--- Project/Logic/TableLogic.cs
+++ Project/Logic/TableLogic.cs
@@ -1,23 +1,76 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using Microsoft.VisualBasic;
 
 public class TableLogic<T>
 {
     public static int tableWidth = 145;
+    public static int selectedOption = 1;
 
     public void PrintTable(List<string> Header, IEnumerable<T> Data, Func<T, List<string>> GenerateRow)
     {
-        PrintLine();
-        PrintRow(Header);
-        PrintLine();
-        foreach (T obj in Data)
-        {
-            PrintRow(GenerateRow(obj));
-            PrintLine();
-        }
-    }
+        ConsoleKeyInfo keyInfo;
+        List<string> geselecteerdeRow = new List<string>();
 
+        do
+        {
+            Console.Clear(); // Clear the console to redraw the table
+
+            PrintLine();
+            PrintRow(Header);
+            PrintLine();
+
+            int rowNumber = 1;
+            foreach (T obj in Data)
+            {
+               if (rowNumber == selectedOption)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    geselecteerdeRow = GenerateRow(obj);
+                    PrintRow(geselecteerdeRow);
+                    Console.ResetColor();
+                    PrintLine();
+                }
+                else
+                {
+                    PrintRow(GenerateRow(obj));
+                    PrintLine();
+                }
+                rowNumber++;
+            }
+            SelectionExplanation();
+
+            keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedOption = Math.Max(1, selectedOption - 1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    selectedOption = Math.Min(Data.Count(), selectedOption + 1);
+                    break;
+                case ConsoleKey.Enter:
+                    Console.Clear(); // Console leegmaken voordat je de rij bewerkt
+                    Console.WriteLine("Bewerk de geselecteerde rij..."); 
+                    PrintLine();
+                    PrintRow(Header);
+                    PrintLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    PrintRow(geselecteerdeRow);
+                    Console.ResetColor();
+                    PrintLine();
+
+                    Console.WriteLine("Klik op een knop om verder te gaan...");
+                    Console.ReadKey(true);
+                    break;
+            }
+        } while (keyInfo.Key != ConsoleKey.Backspace);
+        Console.WriteLine("U keert terug naar het menu...");
+    }
+        
+
+           
     private static void PrintLine()
     {
         Console.WriteLine(new string('-', tableWidth));
@@ -48,5 +101,15 @@ public class TableLogic<T>
         {
             return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
         }
+    }
+
+    private static void SelectionExplanation(){
+        Console.WriteLine("Selecteer een rij doormiddel van de pijltjes.");
+        Console.Write("Als je de juiste rij hebt geselecteerd klik op ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Enter");
+        Console.ResetColor();
+        Console.Write(" om de rij te bewerken.");
+
     }
 }
