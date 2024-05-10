781cf87948e3e07b72a4ffc9cce3da8903be5bf3
Amer Alhasoun <ameralhasoun@gmail.com>
2024-05-10 14:40:47 +0200

    Helper class

--- Project_test/StopToRouteTest.cs
+++ Project_test/StopToRouteTest.cs
@@ -11,13 +11,23 @@ public class StopToRouteTest
         RouteModel routeModel = new RouteModel(1, 1, "Euromast");
         List<StopModel> testList = new(){stopModel};
 
+        StopModel stopModel2 = new StopModel(2, "Blaak");
+        RouteModel routeModel2 = new RouteModel(2, 2, "Noord");
+        List<StopModel> testList2 = new(){stopModel};
+
         //Act
         RouteModel newRoute = RouteLogic.AddToRoute(stopModel, routeModel);
+        RouteModel newRoute2 = RouteLogic.AddToRoute(stopModel2, routeModel2);
 
         //Assert
         for (int i = 0; i < testList.Count; i++)
         {
             Assert.AreEqual(newRoute.Stops[i], testList[i]);
+        }
+
+        for (int i = 0; i < testList.Count; i++)
+        {
+            Assert.AreEqual(newRoute2.Stops[i], testList2[i]);
         }
     }
     
