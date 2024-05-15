9be4f0df4c03bb4964d427a01d7b35bbfaba9d1b
Stéphane Dauvillier <dauvillier04@gmail.com>
2024-05-15 11:59:54 +0200

    Admin standaard toevoegen

--- Project/Presentation/UserLogin.cs
+++ Project/Presentation/UserLogin.cs
@@ -16,27 +16,36 @@ public static class UserLogin
 
         if (acc != null && acc.IsAdmin == false)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Welkom " + acc.FullName);
 
             //Write some code to go back to the menu
             Thread.Sleep(3000);
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("U gaat nu naar de startpagina voor klanten.\n");
+            Console.ResetColor();
             Thread.Sleep(3000);
             CustomerStartMenu.Start();
         }
         else if (acc != null && acc.IsAdmin == true)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Welkom " + acc.FullName);
+            Console.ResetColor();
 
             //Write some code to go back to the menu
             Thread.Sleep(3000);
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("U gaat nu naar de startpagina voor admins.\n");
+            Console.ResetColor();
             Thread.Sleep(3000);
             AdminStartMenu.Start();
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Geen account gevonden met die email en wachtwoord combinatie.");
+            Console.ResetColor();
             Thread.Sleep(3000);
         }
     }
