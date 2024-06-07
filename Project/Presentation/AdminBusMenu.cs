1cc8ff48ad7f9dfd72c718c44416c2b678d86d62
Roel van Loon <contact@roelvanloon.com>
2024-06-07 09:49:39 +0200

    Ga maar verder amer

--- Project/Presentation/AdminBusMenu.cs
+++ Project/Presentation/AdminBusMenu.cs
@@ -30,12 +30,13 @@ public static class AdminBusMenu
 
         if (listAllBusses == null || listAllBusses.Count == 0)
         {
-            BusModel newBusModel = new(busLogic.GenerateNewId(),"test","",false);
+            BusModel newBusModel = new(busLogic.GenerateNewId(),"Plebs","",false);
             SeatModel[,] seatModels = new SeatModel[7, 14];
             seatLogic.CreateSeats(seatModels); // seatModels wordt gevuld met stoelen
             Dictionary<(int Row, int Col), SeatModel> seatingMap = seatLogic.ConvertToDict(seatModels); // seatModels wordt geconvert naar Dictionary
             newBusModel.AddSeatingMap(seatingMap);
             busLogic.UpdateList(newBusModel);
+            
         }
      
         while(true){
@@ -51,12 +52,83 @@ public static class AdminBusMenu
                 List<string> SelectedRow = TableInfo.Value.SelectedRow;
                 if(selectedRowIndex ==  listAllBusses.Count())
                 {
-                    BusModel newBusModel = new(busLogic.GenerateNewId(),"test","",false);
-                    SeatModel[,] seatModels = new SeatModel[7, 14];
-                    seatLogic.CreateSeats(seatModels); // seatModels wordt gevuld met stoelen
-                    Dictionary<(int Row, int Col), SeatModel> seatingMap = seatLogic.ConvertToDict(seatModels); // seatModels wordt geconvert naar Dictionary
-                    newBusModel.AddSeatingMap(seatingMap);
-                    busLogic.UpdateList(newBusModel);
+                    Console.Clear();
+                    int selectedOption = 1; // Default selected option
+
+                    // Display options
+                    DisplayOptions(selectedOption);
+
+                    while (true)
+                    {
+                        // Wait for key press
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                        // Check arrow key presses
+                        switch (keyInfo.Key)
+                        {
+                            case ConsoleKey.UpArrow:
+                                // Move to the previous option
+                                selectedOption = Math.Max(1, selectedOption - 1);
+                                break;
+                            case ConsoleKey.DownArrow:
+                                // Move to the next option
+                                selectedOption = Math.Min(5, selectedOption + 1);
+                                break;
+                            case ConsoleKey.Enter:
+                                Console.Clear();
+                                // Perform action based on selected option (e.g., execute corresponding function)
+                                switch (selectedOption)
+                                {
+                                    case 1:
+                                        BusModel newBusBusiness = new(busLogic.GenerateNewId(),"Business","",false);
+                                        SeatModel[,] seatModelsBusiness  = new SeatModel[7, 12];
+                                        seatLogic.CreateBusinessSeats(seatModelsBusiness);
+                                        // Dictionary<(int Row, int Col), SeatModel> seatingMapBusiness = seatLogic.ConvertToDict(seatModelsBusiness); // seatModels wordt geconvert naar Dictionary
+                                        // newBusBusiness.AddSeatingMap(seatingMapBusiness);
+                                        // busLogic.UpdateList(newBusBusiness);
+                                        ShowAllBusInformation();
+                                        break;
+                                    case 2:
+                                        BusModel newBusModelPlebs = new(busLogic.GenerateNewId(),"Plebs","",false);
+                                        SeatModel[,] seatModelsPlebs = new SeatModel[7, 14];
+                                        seatLogic.CreateSeats(seatModelsPlebs); // seatModels wordt gevuld met stoelen
+                                        Dictionary<(int Row, int Col), SeatModel> seatingMapPlebs = seatLogic.ConvertToDict(seatModelsPlebs); // seatModels wordt geconvert naar Dictionary
+                                        newBusModelPlebs.AddSeatingMap(seatingMapPlebs);
+                                        busLogic.UpdateList(newBusModelPlebs);
+                                        ShowAllBusInformation();
+                                        break;
+                                }
+                                break;
+                        }
+
+                        // Clear console and display options
+                        Console.Clear();
+                        DisplayOptions(selectedOption);
+                    }
+
+                    static void DisplayOptions(int selectedOption)
+                    {
+                        Console.WriteLine("Wat voor bus indeeling wilt u?\n");
+
+                        // Display option 1
+                        Console.ForegroundColor = selectedOption == 1 ? ConsoleColor.Green: ConsoleColor.White;
+                        Console.Write(selectedOption == 1 ? ">> " : "   ");
+                        Console.WriteLine("Business/Party.");
+
+                        // Display option 2
+                        Console.ForegroundColor = selectedOption == 2 ? ConsoleColor.Green : ConsoleColor.White;
+                        Console.Write(selectedOption == 2 ? ">> " : "   ");
+                        Console.WriteLine("Normaal (Plebs).");
+
+                        // Reset text color
+                        Console.ResetColor();
+
+                        Console.Write("\nKlik");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(" Enter ");
+                        Console.ResetColor();
+                        Console.WriteLine("om een optie te selecteren");
+                    }
                     continue;
                 }
                 while(true){
@@ -109,75 +181,7 @@ public static class AdminBusMenu
                             //     break;
                             //     }
                             // }
-                            Console.Clear();
-                            int selectedOption = 1; // Default selected option
-
-                            // Display options
-                            DisplayOptions(selectedOption);
-
-                            while (true)
-                            {
-                                // Wait for key press
-                                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-                                // Check arrow key presses
-                                switch (keyInfo.Key)
-                                {
-                                    case ConsoleKey.UpArrow:
-                                        // Move to the previous option
-                                        selectedOption = Math.Max(1, selectedOption - 1);
-                                        break;
-                                    case ConsoleKey.DownArrow:
-                                        // Move to the next option
-                                        selectedOption = Math.Min(5, selectedOption + 1);
-                                        break;
-                                    case ConsoleKey.Enter:
-                                        Console.Clear();
-                                        // Perform action based on selected option (e.g., execute corresponding function)
-                                        switch (selectedOption)
-                                        {
-                                            case 1:
-                                                listAllBusses[selectedRowIndex].Seats = Convert.ToString("Business");
-                                                busLogic.UpdateList(listAllBusses[selectedRowIndex]);
-                                                ShowAllBusInformation ();
-                                                break;
-                                            case 2:
-                                                listAllBusses[selectedRowIndex].Seats = Convert.ToString("Plebs");
-                                                busLogic.UpdateList(listAllBusses[selectedRowIndex]);
-                                                ShowAllBusInformation ();
-                                                break;
-                                        }
-                                        break;
-                                }
-
-                                // Clear console and display options
-                                Console.Clear();
-                                DisplayOptions(selectedOption);
-                            }
-
-                            static void DisplayOptions(int selectedOption)
-                            {
-                                Console.WriteLine("Wat voor bus indeeling wilt u?\n");
-
-                                // Display option 1
-                                Console.ForegroundColor = selectedOption == 1 ? ConsoleColor.Green: ConsoleColor.White;
-                                Console.Write(selectedOption == 1 ? ">> " : "   ");
-                                Console.WriteLine("Business/Party.");
-
-                                // Display option 2
-                                Console.ForegroundColor = selectedOption == 2 ? ConsoleColor.Green : ConsoleColor.White;
-                                Console.Write(selectedOption == 2 ? ">> " : "   ");
-                                Console.WriteLine("Normaal (Plebs).");
-
-                                // Reset text color
-                                Console.ResetColor();
-
-                                Console.Write("\nKlik");
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.Write(" Enter ");
-                                Console.ResetColor();
-                                Console.WriteLine("om een optie te selecteren");
-                            }
+                           break;
                         }
 
                     
