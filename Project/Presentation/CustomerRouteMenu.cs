1e77c105478ac6b1488d8a6c469937ba0e7f1824
Amer Alhasoun <ameralhasoun@gmail.com>
2024-06-11 13:37:53 +0200

    Console.WriteLine in CusterrouteMenu --> ColorPrint.PrintYellow

--- Project/Presentation/CustomerRouteMenu.cs
+++ Project/Presentation/CustomerRouteMenu.cs
@@ -232,7 +232,7 @@ public static class CustomerRouteMenu
             else
             {
                 Console.Clear();
-                Console.WriteLine("U gaat terug naar het startmenu");
+                ColorPrint.PrintYellow("U gaat terug naar het startmenu");
                 Thread.Sleep(3000);
                 CustomerStartMenu.Start();
                 break;
